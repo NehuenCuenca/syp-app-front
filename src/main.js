8bb7b79837f5bf809dbdce92151f77e5a0e16730
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-10-03 18:46:23 -0300

    feat(theme): aplicar tema oscuro y ajustar colores de fondo en la aplicación

--- src/main.js
+++ src/main.js
@@ -1,65 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
-import { definePreset } from '@primeuix/themes';
-import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
 import ToastService from 'primevue/toastservice';
 import './style.css'
 import App from './App.vue'
 import router from './router'
 import './plugins/axios'
 import { useAuthStore } from './stores/auth'
-
-
-const MyPreset = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: '{indigo.50}',
-            100: '{indigo.100}',
-            200: '{indigo.200}',
-            300: '{indigo.300}',
-            400: '{indigo.400}',
-            500: '{indigo.500}',
-            600: '{indigo.600}',
-            700: '{indigo.700}',
-            800: '{indigo.800}',
-            900: '{indigo.900}',
-            950: '{indigo.950}'
-        },
-        colorScheme: {
-            light: {
-                surface: {
-                    0: '#ffffff',
-                    50: '{zinc.50}',
-                    100: '{zinc.100}',
-                    200: '{zinc.200}',
-                    300: '{zinc.300}',
-                    400: '{zinc.400}',
-                    500: '{zinc.500}',
-                    600: '{zinc.600}',
-                    700: '{zinc.700}',
-                    800: '{zinc.800}',
-                    900: '{zinc.900}',
-                    950: '{zinc.950}'
-                },
-                // primary: {},
-                // highlight: {}
-            },
-        }
-    },
-    components: {
-        fieldset: {
-            legend: {    
-                background: '{primary.200}',
-            }
-        },
-        inputtext: {
-            root: {
-                background: '{surface.100}',
-            }
-        },
-    },
-});
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -69,12 +17,11 @@ app.use(router)
 app.use(ToastService)
 app.use(PrimeVue, {
     theme: {
-        preset: MyPreset,
+        preset: Lara,
         options: {
             prefix: 'p',
-            darkModeSelector: false || 'none',
+            darkModeSelector: '.my-app-dark',
         },
-
     }
 })
 
