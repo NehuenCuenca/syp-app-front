6dd0e52b3b6056c4a8491575c3d57c794786cb4b
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-10-20 10:57:28 -0300

    feat(config): reemplazar tsconfig.json por jsconfig.json y actualizar alias en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -31,7 +31,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '~composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
+      '~views': fileURLToPath(new URL('./src/views', import.meta.url)),
+    },
+  },
 })
