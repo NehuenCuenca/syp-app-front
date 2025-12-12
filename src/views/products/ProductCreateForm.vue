88ac7a8210a2f277412cce076e560b5acb7ec44c
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-12-12 11:01:52 -0300

    feat(validation and styles): replace and improve styles at ProductCreateForm modal using tailwind. Increase fontsize of errors. Add validation to category and current stock fields

--- src/views/products/ProductCreateForm.vue
+++ src/views/products/ProductCreateForm.vue
@@ -1,80 +1,93 @@
 <!-- ProductCreateForm.vue -->
 <!-- Formulario para crear un nuevo producto -->
 <template>
-  <div class="product-create-form">
-    <div class="form-content">
+  <div class="flex items-center flex-col gap-4">
+    <div class="flex justify-between max-w-xl flex-wrap gap-6">
       <!-- Campo: Nombre del producto -->
+    <div class="flex flex-col flex-[1_2_200px] gap-y-2">
       <InputGroup>
         <InputGroupAddon>
             <i class="pi pi-th-large"></i>
         </InputGroupAddon>
         <FloatLabel variant="in">
-          <InputText id="name" v-model="formData.name" :class="{ 'p-invalid': errors.name }"/>
+          <InputText id="name" v-model="formData.name" :invalid="!!errors.name"/>
           <label for="name">NOMBRE DE PRODUCTO</label>
         </FloatLabel>
-        <Message v-if="errors.name" severity="error" variant="simple" size="small" class="p-error">{{ errors.name }}</Message>
       </InputGroup>
+      <Message v-if="errors.name" severity="error" variant="simple" size="large" class="block">{{ errors.name }}</Message>
+    </div>
+
 
       <!-- Campo: categoria -->
+    <div class="flex flex-col flex-[1_2_200px] gap-y-2">
       <InputGroup>
         <InputGroupAddon>
             <i class="pi pi-tags"></i>
         </InputGroupAddon>
         <FloatLabel variant="in">
-          <AutoComplete showClear id="category" v-model="formData.category" dropdown :suggestions="categories" @complete="searchCategory" optionLabel="search_alias" optionValue="name" @item-select="(e) => formData.category = e.value.name" emptySearchMessage="No se encontraron sugerencias."/>
+          <AutoComplete showClear id="category" v-model="formData.category" dropdown :suggestions="categories" @complete="searchCategory" optionLabel="search_alias" optionValue="name" @item-select="(e) => formData.category = e.value.name" emptySearchMessage="No se encontraron sugerencias." :invalid="!!errors.category"/>
           <label for="category">CATEGORIA</label>
         </FloatLabel>
-        <Message v-if="errors.category" severity="error" variant="simple" size="small" class="p-error">{{ errors.category }}</Message>
       </InputGroup>
+      <Message v-if="errors.category" severity="error" variant="simple" size="large" class="p-error">{{ errors.category }}</Message>
+    </div>
 
       <!-- Campo: Precio de COMPRA-->
+    <div class="flex flex-col flex-[1_2_200px] gap-y-2">
       <InputGroup>
         <InputGroupAddon>
             <i class="pi pi-dollar"></i>
         </InputGroupAddon>
         <FloatLabel variant="in">
           <InputNumber id="buy_price" v-model="formData.buy_price"
-          prefix="$" :class="{ 'p-invalid': errors.buy_price }"
+          prefix="$" :invalid="!!errors.buy_price"
           :min="0"
-        />
-          <label for="buy_price">PRECIO DE COMPRA</label>
+          />
+          <label for="buy_price" >PRECIO DE COMPRA</label>
         </FloatLabel>
-        <Message v-if="errors.buy_price" severity="error" variant="simple" size="small" class="p-error">{{ errors.buy_price }}</Message>
       </InputGroup>
+      <Message v-if="errors.buy_price" severity="error" variant="simple" size="large" class="p-error">{{ errors.buy_price }}</Message>
+    </div>
 
       <!-- Campo: ganancia -->
+    <div class="flex flex-col flex-[1_2_200px] gap-y-2">
       <InputGroup>
         <InputGroupAddon>
             <i class="pi pi-percentage"></i>
         </InputGroupAddon>
         <FloatLabel variant="in">
           <InputNumber id="profit_percentage" v-model="formData.profit_percentage"
-          suffix="%" :class="{ 'p-invalid': errors.profit_percentage }"
+          suffix="%" :invalid="!!errors.profit_percentage"
           :min="1" />
           <label for="profit_percentage">GANANCIA (%)</label>
         </FloatLabel>
-        <Message v-if="errors.profit_percentage" severity="error" variant="simple" size="small" class="p-error">{{ errors.profit_percentage }}</Message>
       </InputGroup>
+      <Message v-if="errors.profit_percentage" severity="error" variant="simple" size="large" class="p-error">{{ errors.profit_percentage }}</Message>
+    </div>
 
       <!-- Campo: precio de venta -->
+    <div class="flex flex-col flex-[1_2_200px] gap-y-2">
+
       <InputGroup>
         <InputGroupAddon>
             <i class="pi pi-dollar"></i>
         </InputGroupAddon>
         <FloatLabel variant="in">
           <InputNumber id="sale_price" v-model="formData.sale_price"
-            prefix="$" :class="{ 'p-invalid': errors.sale_price }"
+            prefix="$" :invalid="!!errors.sale_price"
             :min="0" />
           <label for="sale_price">PRECIO DE VENTA</label>
         </FloatLabel>
-        <Message v-if="errors.sale_price" severity="error" variant="simple" size="small" class="p-error">{{ errors.sale_price }}</Message>
       </InputGroup>
+      <Message v-if="errors.sale_price" severity="error" variant="simple" size="large" class="p-error">{{ errors.sale_price }}</Message>
+    </div>
 
       <!-- Campo: stock inicial -->
+    <div class="flex flex-col flex-[1_2_200px] gap-y-2">
       <InputGroup>
         <FloatLabel variant="in">
           <InputNumber id="current_stock" v-model="formData.current_stock"
-          :class="{ 'p-invalid': errors.current_stock }" showButtons
+          :invalid="!!errors.current_stock" showButtons
           buttonLayout="horizontal" :step="5"
           :min="1" 
           >
@@ -87,16 +100,17 @@
           </InputNumber>
           <label for="current_stock">STOCK INICIAL</label>
         </FloatLabel>
-        <Message v-if="errors.current_stock" severity="error" variant="simple" size="small" class="p-error">{{ errors.current_stock }}</Message>
       </InputGroup>
+      <Message v-if="errors.current_stock" severity="error" variant="simple" size="large" class="p-error">{{ errors.current_stock }}</Message>
+    </div>
 
       <!-- Campo: Alerta minimo de stock -->
+    <div class="flex flex-col flex-[0_1_250px]">
       <InputGroup>
         <FloatLabel variant="in">
           <InputNumber id="min_stock_alert" v-model="formData.min_stock_alert"
-          :class="{ 'p-invalid': errors.min_stock_alert }" showButtons
-          buttonLayout="horizontal" :step="5"
-          :min="1" 
+          showButtons buttonLayout="horizontal" :step="5"
+          :min="1" :invalid="!!errors.min_stock_alert"
           >
             <template #incrementbuttonicon>
                 <span class="pi pi-plus" />
@@ -107,25 +121,27 @@
           </InputNumber>
           <label for="min_stock_alert">ALERTA DE STOCK BAJO</label>
         </FloatLabel>
-        <Message v-if="errors.min_stock_alert" severity="error" variant="simple" size="small" class="p-error">{{ errors.min_stock_alert }}</Message>
       </InputGroup>
-
-      <Message severity="error" v-if="error">{{ error }}</Message>
+      <Message v-if="errors.min_stock_alert" severity="error" variant="simple" size="large" class="p-error">{{ errors.min_stock_alert }}</Message>
     </div>
 
-    <!-- Botones de acción -->
-    <div class="form-actions">
+      <Message severity="error" v-if="error">{{ error }}</Message>
+      <!-- Botones de acción -->
+    <div class="w-full flex justify-around">
       <Button
         label="Cancelar"
         severity="secondary"
         outlined
         @click="handleCancel"
       />
+
       <Button
         label="Guardar"
         icon="pi pi-check"
         @click="handleSubmit"
+        ref="submitButton"
       />
+    </div>
     </div>
   </div>
 </template>
@@ -193,10 +209,15 @@ const validateForm = () => {
     errors.value.name = 'El nombre es requerido';
     isValid = false;
   }
+  
+  if (!formData.category || formData.category.trim() === '') {
+    errors.value.category = 'La categoria es requerida';
+    isValid = false;
+  }
 
   // Validar precio compra (requerido y mayor a 0)
   if (!formData.buy_price || formData.buy_price <= 0) {
-    errors.value.buy_price = 'El precio compra debe ser mayor a 0';
+    errors.value.buy_price = 'El precio compra debe ser mayor a $0';
     isValid = false;
   }
 
@@ -208,11 +229,16 @@ const validateForm = () => {
 
   // Validar precio venta(requerido y mayor a 0)
   if (!formData.sale_price || formData.sale_price <= 0) {
-    errors.value.sale_price = 'El precio venta debe ser mayor a 0';
+    errors.value.sale_price = 'El precio venta debe ser mayor a $0';
     isValid = false;
   }
 
-  // Validar precio (requerido y mayor a 0)
+
+  if (!formData.current_stock || formData.current_stock <= 0) {
+    errors.value.current_stock = 'El stock inicial debe ser igual o mayor a 1';
+    isValid = false;
+  }
+
   if (!formData.min_stock_alert || formData.min_stock_alert <= 0) {
     errors.value.min_stock_alert = 'El alerta de stock minimo debe ser igual o mayor a 1';
     isValid = false;
@@ -245,9 +271,7 @@ const handleSubmit = async() => {
 };
 
 // Maneja la cancelación
-const handleCancel = () => {
-  emit('close');
-};
+const handleCancel = () => { emit('close'); };
 
 const calculateSalePrice = () => {
     formData.sale_price = parseInt((formData.buy_price * (formData.profit_percentage/100 + 1)).toFixed(0));
@@ -257,58 +281,4 @@ watch( () => formData.buy_price, calculateSalePrice );
 watch( () => formData.profit_percentage, calculateSalePrice );
 </script>
 
-<style scoped>
-.product-create-form {
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-}
-
-.form-content {
-  display: flex;
-  /* flex-direction: column; */
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 1.25rem;
-}
-
-.field {
-  display: flex;
-  flex-direction: column;
-  flex: 1 1 auto;
-  gap: 0.5rem;
-}
-
-label {
-  font-weight: 600;
-  font-size: 0.95rem;
-  color: #495057;
-}
-
-label.required::after {
-  content: ' *';
-  color: #e24c4c;
-}
-
-.p-error {
-  color: #e24c4c;
-  font-size: 0.875rem;
-  margin-top: 0.25rem;
-}
-
-.form-actions {
-  display: flex;
-  justify-content: flex-end;
-  gap: 0.75rem;
-  padding-top: 1rem;
-  border-top: 1px solid #e5e7eb;
-}
-
-.w-full {
-  width: 100%;
-}
-
-.w-half {
-  width: 50%;
-}
-</style>
+<style scoped></style>
