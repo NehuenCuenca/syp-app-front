ff35c3c2fac4520f7ba68bc26dd26b86c707dd90
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-12-27 20:48:02 -0300

    refactor(helper function): change format of extracted date used at UniversalCard

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -4,5 +4,5 @@
 export function extractDate(datetime) {
     // datetime = "2025-12-08 18:51:41"
     const [date] = datetime.split(" ");
-    return date; // "2025-12-08"
+    return date.split('-').reverse().join('/'); // "2025-12-08"
 }
